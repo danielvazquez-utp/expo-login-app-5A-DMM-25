5d68a5ca3b6f6c2e6b457f9f0d4e4e3bbd1959fe
Paulo Daniel Vázquez Mora <daniel.vazquez@utpuebla.edu.mx>
2025-03-04 23:30:16 -0600

    Componente DrawerNavigator agregado

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,57 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Dashboard } from '../screens';
+import { ArticlesScreen } from '../screens/ArticlesScreen';
+import { PersonScreen } from '../screens/PersonScreen';
+import { PlaceScreen } from '../screens/PlaceScreen';
+import { AssignmentScreen } from '../screens/AssignmentScreen';
+import { InventoryScreen } from '../screens/InventoryScreen';
+import { ScanScreen } from '../screens/ScanScreen';
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen
+          name="dashbord"
+          component={ Dashboard }
+          options={{ title: 'Menú principal' }}
+        />
+        <Drawer.Screen
+          name="article"
+          component={ ArticlesScreen }
+          options={{ title: 'Artículos' }}
+        />
+        <Drawer.Screen
+          name="person"
+          component={ PersonScreen }
+          options={{ title: 'Personas' }}
+        />
+        <Drawer.Screen
+          name="places"
+          component={ PlaceScreen }
+          options={{ title: 'Ubicaciones' }}
+        />
+        <Drawer.Screen
+          name="assigments"
+          component={ AssignmentScreen }
+          options={{ title: 'Asignaciones' }}
+        />
+        <Drawer.Screen
+          name="inventary"
+          component={ InventoryScreen }
+          options={{ title: 'Inventarios' }}
+        />
+        <Drawer.Screen
+          name="scan"
+          component={ ScanScreen }
+          options={{ title: 'Escanear' }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default DrawerNavigator;
